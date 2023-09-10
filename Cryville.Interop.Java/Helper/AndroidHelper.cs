9aa0af14aea2b05a89805c42cfbe4979a005f93c
PopSlime <popslime@qq.com>
2023-09-10 13:50:39 +0800

    Add Android helper.

---
+++ Cryville.Interop.Java/Helper/AndroidHelper.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Cryville.Interop.Java.Helper {
+	/// <summary>
+	/// Provides a set of helper methods for Android.
+	/// </summary>
+	public static class AndroidHelper {
+		static IJniEnv GetEnv() {
+			JavaVMManager.Instance.CurrentVM.GetEnv(out var env, 0x00010006);
+			return env;
+		}
+
+		[ThreadStatic]
+		static JniValue[] args0 = new JniValue[0];
+		/// <summary>
+		/// Gets a local reference to the current <c>android.app.Application</c> object.
+		/// </summary>
+		/// <returns>A local reference to the current <c>android.app.Application</c> object.</returns>
+		/// <remarks>
+		/// <para>This method can only be called from the main thread, otherwise it returns a reference to <c>null</c>.</para>
+		/// </remarks>
+		public static IntPtr GetCurrentApplication() {
+			IJniEnv env = GetEnv();
+			IntPtr result = IntPtr.Zero;
+			env.PushLocalFrame(2);
+			try {
+				var c = env.FindClass("android/app/ActivityThread");
+				if (c == IntPtr.Zero) throw new InvalidOperationException("Could not find the Java class android.app.ActivityThread.");
+				var m = env.GetStaticMethodID(c, "currentApplication", "Landroid/app/Application;");
+				if (m == IntPtr.Zero) throw new InvalidOperationException("Could not find the method currentApplication() on Java class android.app.ActivityThread.");
+				result = env.CallStaticObjectMethod(c, m, args0);
+			}
+			finally {
+				result = env.PopLocalFrame(result);
+			}
+			return result;
+		}
+	}
+}
