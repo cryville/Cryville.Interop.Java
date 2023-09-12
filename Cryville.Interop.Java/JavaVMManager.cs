58cb675634edb486c885a445b30610638f02c872
PopSlime <popslime@qq.com>
2023-09-12 19:40:10 +0800

    Make VM manager static.

--- Cryville.Interop.Java/JavaVMManager.cs
+++ Cryville.Interop.Java/JavaVMManager.cs
@@ -1,44 +1,37 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Cryville.Interop.Java {
 	/// <summary>
 	/// Java VM manager.
 	/// </summary>
-	public class JavaVMManager {
-		static JavaVMManager m_instance;
-		/// <summary>
-		/// An instance of the <see cref="JavaVMManager" /> singleton class.
-		/// </summary>
-		public static JavaVMManager Instance {
-			get {
-				if (m_instance == null)
-					m_instance = new JavaVMManager();
-				return m_instance;
-			}
-		}
-		JavaVMManager() { }
-
+	public static class JavaVMManager {
 		/// <summary>
 		/// Registers a Java VM.
 		/// </summary>
 		/// <param name="vm">A Java VM.</param>
 		/// <exception cref="ArgumentNullException"><paramref name="vm" /> is <see langword="null" />.</exception>
-		public void Register(IJniInvoke vm) {
+		public static void Register(IJniInvoke vm) {
 			if (vm == null) throw new ArgumentNullException(nameof(vm));
 			if (m_vms.Contains(vm)) return;
 			m_vms.Add(vm);
+			if (m_currentVM == null) m_currentVM = vm;
 		}
 
-		readonly List<IJniInvoke> m_vms = new List<IJniInvoke>();
+		static readonly List<IJniInvoke> m_vms = new List<IJniInvoke>();
 		/// <summary>
 		/// All registered Java VMs.
 		/// </summary>
-		public IEnumerable<IJniInvoke> VMs => m_vms;
+		public static IEnumerable<IJniInvoke> VMs => m_vms;
+
+		static IJniInvoke m_currentVM;
 		/// <summary>
-		/// The current VM.
+		/// The first VM registered.
 		/// </summary>
-		public IJniInvoke CurrentVM => m_vms.Single();
+		public static IJniInvoke CurrentVM => m_currentVM;
+		/// <summary>
+		/// The <see cref="IJniEnv" /> of the first VM registered.
+		/// </summary>
+		public static IJniEnv CurrentEnv => CurrentVM.GetEnv(0x00010006);
 	}
 }
