c15aec7a5981a677eeebcc1e7ca71576a3232944
PopSlime <popslime@qq.com>
2025-02-01 20:21:42 +0800

    feat(objectstream): Implement ObjectStreamContext

---
+++ Cryville.Interop.Java.ObjectStream/ObjectStreamContext.cs
@@ -0,0 +1,25 @@
+using Cryville.Common.Compat;
+using System;
+using System.Collections.Generic;
+
+namespace Cryville.Interop.Java.ObjectStream {
+	public sealed class ObjectStreamContext {
+		readonly Dictionary<SerializedJavaClassDesc, Dictionary<string, int>> _fieldMap = [];
+		public object? GetValue(SerializedJavaObject obj, string fieldName) {
+			ThrowHelper.ThrowIfNull(obj, nameof(obj));
+			var classDesc = obj.ClassDesc ?? throw new ArgumentException("Missing class description.", nameof(obj));
+			if (!_fieldMap.TryGetValue(classDesc, out var map)) _fieldMap.Add(classDesc, map = []);
+			if (!map.TryGetValue(fieldName, out var index)) {
+				for (int i = 0; i < classDesc.Fields.Count; i++) {
+					if (classDesc.Fields[i].Name == fieldName) {
+						map.Add(fieldName, index = i);
+						goto result;
+					}
+				}
+				throw new InvalidOperationException("Field not found.");
+			}
+		result:
+			return obj.Values[index];
+		}
+	}
+}
